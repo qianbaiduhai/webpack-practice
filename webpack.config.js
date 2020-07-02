f33ced3455cf5a405c1ab3ef3f213ebefb19c8f2
qianbaiduhai <qianbaiduhai@163.com>
2020-07-02 18:20:09 +0800

    feat: 添加PostCss,px2vw的移动端适配

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     'style-loader',
-                    'css-loader'
+                    'css-loader',
+                    'postcss-loader'
                 ]
             }
         ]
