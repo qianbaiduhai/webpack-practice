a9ef6db2d86b1dc901721cef96ed42b13c3e97d7
qianbaiduhai <qianbaiduhai@163.com>
2020-07-02 17:43:22 +0800

    step2: 加载CSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
+import './style.css'
 
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['hello', 'webpack'], ' ')
+    element.classList.add('hello')
+
     return element
 }
 
