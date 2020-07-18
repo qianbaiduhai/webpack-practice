466e724525c2f519a6e0690173dd377cbeea8c02
qianbaiduhai <qianbaiduhai@163.com>
2020-07-18 09:58:20 +0800

    使用webpack-dev-server

--- src/print.js
+++ src/print.js
@@ -1,3 +1,3 @@
 export default function printMe() {
-    console.log('我是来自print.js文件,不是吧，怎么删除了我的模版文件');
+    console.log('我是来自print.js文件,不是吧，怎么删除了我的模版文件1');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
         print: './src/print.js'
     },
     devtool: "inline-source-map",
+    devServer: {
+        contentBase: './dist'
+    },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
@@ -17,6 +20,5 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    },
-
+    }
 }
