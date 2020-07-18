20034f3b222e06d3a4b7e1ba86df22d15b6c20b1
qianbaiduhai <qianbaiduhai@163.com>
2020-07-18 17:46:08 +0800

    添加通用模块

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import _ from 'lodash';
+import { cube } from './math'
 
 function component() {
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['hello', 'webpack'], ' ')
+    var element = document.createElement('pre');
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
     return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,9 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    optimization: {
+        usedExports: true
     }
 }
